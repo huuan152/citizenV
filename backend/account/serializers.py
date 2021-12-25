08afd8d937908d1e34717baca333a0faf3ee6b3d
vuongvanchinh <vuongvanchinh2001@gmail.com>
2021-12-25 19:38:30 +0700

    actualy permission

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User
+from . import CadreLevels
 from datetime import datetime, timedelta
 from pytz import timezone
 
@@ -37,15 +38,27 @@ class UserLoginSerializer(serializers.ModelSerializer):
     operate_from = serializers.DateTimeField(format='%Y-%m-%dT%H:%M:%S:%f')
     operate_to = serializers.DateTimeField(format='%Y-%m-%dT%H:%M:%S:%f')
     agency = serializers.SerializerMethodField()
+    actually_declared_permission = serializers.SerializerMethodField()
+
     class Meta: 
         model = User
-        fields = ['id', 'username', 'level', 'declared_permission','operate_from', 'operate_to', 'supervisor', 'agency']
+        fields = ['id', 'username', 'level', 'declared_permission', 'actually_declared_permission' ,'operate_from', 'operate_to', 'supervisor', 'agency']
     def get_agency(self, instance):
         agency = instance.agency
         if agency:
             return {'id': agency.id, 'name': agency.name, 'completed_declare': agency.completed_declare, 'stringName': instance.agency.__str__()}
 
         return None
+    def get_actually_declared_permission(self, instance):
+        if instance.level == CadreLevels.CENTRAL:
+            return True
+        username = instance.username
+        l = len(username)
+        user_tree = [username[:i] for i in range(2, l, 2)] # 010101 => ['01', '0101', '010101']
+        if User.objects.filter(username__in = user_tree, declared_permission = False).exists():
+            return False
+        return True
+        
 class ScheduleForm(serializers.Serializer):
     operate_from = serializers.DateTimeField(format='%Y-%m-%dT%H:%M:%S:%f')
     operate_to = serializers.DateTimeField(format='%Y-%m-%dT%H:%M:%S:%f')
@@ -68,9 +81,9 @@ class ScheduleForm(serializers.Serializer):
         print(operate_to, operate_from, operate_to < now)
         if operate_to < now:
             print("raise")
-            raise serializers.ValidationError({'operate_to':'operate_to must be in the future'})
+            raise serializers.ValidationError({'operate_to':'phải là một thời điểm ở tương lai'})
         
         if(operate_to < operate_from):
-            raise serializers.ValidationError({'operate_to':'operate_to must be greate than operate_from'})
+            raise serializers.ValidationError({'operate_to':'thời điểm kết thúc phaỉ lớn hơn thời điểm bắt đầu'})
         return dt
     
